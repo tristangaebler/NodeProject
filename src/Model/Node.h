7c99a4d7a3ca196a5a499f4abd9b697465604bf0
tristangaebler <tgae6529@740s-pl-115.local>
2016-01-27 09:31:17 -0700

    Created the NodeProject

---
+++ src/Model/Node.h
@@ -0,0 +1,23 @@
+/*
+ * Node.h
+ *
+ *  Created on: Jan 27, 2016
+ *      Author: tgae6529
+ */
+
+#ifndef MODEL_NODE_H_
+#define MODEL_NODE_H_
+
+template <class Type>
+class Node {
+private:
+	Type value;		//Container for the vale stored in the node.
+	Node * pointers;	//Pointer to the array of Nodes that are linked to the node
+public:
+	Node();
+	virtual ~Node();
+	Type getValue();
+	Node * getPointers();
+};
+
+#endif /* MODEL_NODE_H_ */
