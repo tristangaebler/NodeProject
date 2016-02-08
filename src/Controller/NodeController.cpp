ff73f027ef3f15dac7fc1e98ff706c0863792190
tristangaebler <Tristan.Gaebler@gmail.com>
2016-02-08 09:32:52 -0700

    Added documentation to all my .cpp files in my Controller package

--- src/Controller/NodeController.cpp
+++ src/Controller/NodeController.cpp
@@ -6,20 +6,26 @@
  */
 
 #include "NodeController.h"
-
+/*
+ * This is the Constructor of the NodeController class. We are giving the CTECArray 5 indexes or spots.
+ */
 NodeController::NodeController() {
 	//this->intNode.setValue(5);
 	//this->stringArrayNode.setValue("words are fun");
 
-	myStringArray = new CTECArray<string>(5);
+	myStringArray = new CTECArray<string>(5); //Private CTEC Array made up of Strings
 
 
 }
-
+/*
+ * This is the destructor for the NodeController class. Currently nothing is happening in it. I do not fully understand the destructor.
+ */
 NodeController::~NodeController() {
 	// TODO Auto-generated destructor stub
 }
-
+/*
+ * This is the start method that is void. We are giving the CTECArray values. Then we print the values out using cout <<.
+ */
 void NodeController :: start() {
 	//cout << intNode.getValue() << endl;
 	//cout << stringArrayNode.getValue() << endl;
@@ -31,11 +37,13 @@ void NodeController :: start() {
 	string fifth = "fifth";
 
 	myStringArray -> set(0, first);
+	cout << myStringArray->get(0) << endl;
 	myStringArray -> set(1, second);
 	myStringArray -> set(2, third);
 	myStringArray -> set(3, fourth);
 	myStringArray -> set(4, fifth);
 
+	//Prints out the index 5 times.
 	for(int index = 0; index < myStringArray->getSize(); index++) {
 		cout << "The contents at index " << index << " are: " << myStringArray->get(index) << endl;
 	}
--- src/Controller/NodeRunner.cpp
+++ src/Controller/NodeRunner.cpp
@@ -6,7 +6,9 @@
  */
 
 #include "NodeController.h"
-
+/*
+ * The main method that starts the program. It declares a new NodeController and calls it baseController. BaseController calls start().
+ */
 int main() {
 	NodeController * baseController = new NodeController();
 	baseController->start();
