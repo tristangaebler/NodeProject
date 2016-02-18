da4e16bebdbf82cbdb9c802e928c015630b03492
tristangaebler <Tristan.Gaebler@gmail.com>
2016-02-18 09:17:23 -0700

    Implemented all the methods in the CTECList.cpp file

--- src/Model/CTECList.cpp
+++ src/Model/CTECList.cpp
@@ -6,13 +6,39 @@
  */
 
 #include "CTECList.h"
+#include <assert.h>
+#include <iostream>
 
-CTECList::CTECList() {
-	// TODO Auto-generated constructor stub
+using namespace std;
+
+template<class Type>
+CTECList<Type>::CTECList(int size) {
+
 
 }
 
-CTECList::~CTECList() {
-	// TODO Auto-generated destructor stub
+template <class Type>
+CTECList<Type>::~CTECList() {
+
+}
+
+template <class Type>
+void CTECList<Type>::set(int position, const Type& value) {
+
+}
+
+template <class Type>
+Type CTECList<Type>::get(int position) {
+
+}
+
+template <class Type>
+void CTECList<Type>::add(const Type& value) {
+
+}
+
+template <class Type>
+void CTECList<Type>::deletePos(int position) {
+
 }
 
